a8b1312f6c461755c484dd3188cbb1855f8f2f74
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-12-12 22:15:05 -0800

    minor changes

--- NFT_Trading_Market/src/main/java/edu/sjsu/cmpe275/nft/repos/TransactionRepository.java
+++ NFT_Trading_Market/src/main/java/edu/sjsu/cmpe275/nft/repos/TransactionRepository.java
@@ -16,12 +16,12 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 //	@Query("from Transaction where user_id=:userId and symbol=:currency and transaction_date > clock_timestamp() - make_interval(days => :days)")
 //	List<Transaction> filterTransactions(@Param("userId") Long userId, @Param("currency") String currency, @Param("days") int days);
 	
-	@Query(value = "from Transaction where user_id=:userId and symbol=:currency and transaction_date > DATE_SUB(CURDATE(), INTERVAL 1 DAY)", nativeQuery = true)
+	@Query(value = "from Transaction where user_id=:userId and symbol=:currency and transaction_date >= DATE_SUB(CURDATE(), INTERVAL 1 DAY)", nativeQuery = true)
 	List<Transaction> filterLastOneDayTransactions(@Param("userId") Long userId, @Param("currency") String currency);
 	
-	@Query(value = "from Transaction where user_id=:userId and symbol=:currency and transaction_date > DATE_SUB(CURDATE(), INTERVAL 7 DAY)", nativeQuery = true)
+	@Query(value = "from Transaction where user_id=:userId and symbol=:currency and transaction_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)", nativeQuery = true)
 	List<Transaction> filterLastOneWeekTransactions(@Param("userId") Long userId, @Param("currency") String currency);
 	
-	@Query(value = "from Transaction where user_id=:userId and symbol=:currency and transaction_date > DATE_SUB(CURDATE(), INTERVAL 30 DAY)", nativeQuery = true)
+	@Query(value = "from Transaction where user_id=:userId and symbol=:currency and transaction_date >= DATE_SUB(CURDATE(), INTERVAL 30 DAY)", nativeQuery = true)
 	List<Transaction> filterLastOneMonthTransactions(@Param("userId") Long userId, @Param("currency") String currency);
 }
